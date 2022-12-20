dfbb3240d6736d64886b8143d857b8b796ed3b0e
Nil Borodulia <nil.b@oxtech.org>
2022-12-20 12:23:14 +0300

    add endpoint for send sms

--- app/server/server.go
+++ app/server/server.go
@@ -1,13 +1,20 @@
 package server
 
 import (
-  "log"
+    "io"
+    "log"
    "net/http"
+   "strings"
    "github.com/pkg/errors"
    "github.com/go-chi/chi/v5"
    "github.com/go-chi/chi/v5/middleware"
+   "github.com/go-chi/render"
    "github.com/jtrw/go-rest"
+   lgr "github.com/go-pkgz/lgr"
+   "encoding/json"
 )
+
+type JSON map[string]interface{}
 
 type Server struct {
     Host           string
@@ -34,8 +41,55 @@ func (s Server) routes() chi.Router {
 
     router.Use(middleware.Logger)
     router.Use(rest.Ping)
-    router.Route("/", func(r chi.Router) {
+
+    router.Route("/api/v1", func(r chi.Router) {
+        r.Post("/send/sms", s.sendSms)
+        r.Get("/check/status/*", s.checkStatus)
     })
 
 	return router
+}
+
+func (s Server) sendSms(w http.ResponseWriter, r *http.Request) {
+    b, err := io.ReadAll(r.Body)
+    if err != nil {
+        lgr.Printf("[ERROR] %s", err)
+    }
+    value := string(b)
+    lgr.Printf("[INFO] %s", value)
+
+    //uri := chi.URLParam(r, "*")
+
+    dataJson := &JSON{}
+    //dataType := "text"
+    if isContentTypeJson(r) {
+        errJsn := json.Unmarshal([]byte(value), dataJson)
+        if errJsn != nil {
+            lgr.Printf("ERROR Invalid json in Data");
+            return
+        }
+       // dataType = "json"
+    }
+
+    render.Status(r, http.StatusCreated)
+    render.JSON(w, r, JSON{"status": "ok"})
+    return
+}
+
+func (s Server) checkStatus(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func isContentTypeJson(r *http.Request) bool {
+    return r.Header.Get("Content-Type") == strings.ToLower("application/json")
+}
+
+func getKeyAndBucketByUrl(uri string) (string, string) {
+    chunks := strings.Split(uri, "/")
+
+    length := len(chunks)
+    keyStore := chunks[length-1]
+    bucket := strings.Join(chunks[:length-1], "/")
+
+    return keyStore, bucket
 }
